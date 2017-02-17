b35733367288f9e49078779d4a736ea008678346
DarrienG <darrienglasser@outlook.com>
2017-02-17 13:53:50 -0500

    Add test MongoDB

--- src/main/java/Controller/ApplicationController.java
+++ src/main/java/Controller/ApplicationController.java
@@ -1,11 +1,8 @@
 package Controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+//@SpringBootApplication
 public class ApplicationController {
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationController.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(ApplicationController.class, args);
+//    }
 }
