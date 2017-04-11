4b6c263de841f85a275211c34ef1868f58e5a63c
DarrienG <darrienglasser@outlook.com>
2017-04-11 15:41:46 -0400

    use debug output

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -9,11 +9,14 @@ import Pojo.Credentials;
 import Pojo.SessionId;
 import Pojo.UserToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 
@@ -36,30 +39,29 @@ public class LoginController {
 
     @RequestMapping("/oauth")
     @ResponseBody
-    public UserToken greeting(
-            @RequestParam(value = "grant_type") String grantType,
-            @RequestParam(value = "client_id") String clientId,
-            @RequestParam(value = "client_secret") String clientSecret,
-            @RequestParam(value = "username") String email,
-            @RequestParam(value = "password") String password,
-            @RequestParam(value = "scope") String scope) {
+    public String greeting(
+            HttpServletRequest request,
+            HttpServletResponse response, Model model) {
 
-        if (!this.clientSecret.equals(clientSecret)) {
-            throw new BadSecretException();
-        } else {
-            Credentials credentials = repository.findByEmail(email);
-            if (credentials == null) {
-                System.out.println("Thing is null");
-                throw new UserNotFoundException();
-            }
+//        if (!this.clientSecret.equals(clientSecret)) {
+//            throw new BadSecretException();
+//        } else {
+//            Credentials credentials = repository.findByEmail(email);
+//            if (credentials == null) {
+//                System.out.println("Thing is null");
+//                throw new UserNotFoundException();
+//            }
+//
+//            if (!credentials.getPassword().equals(password)) {
+//                throw new LoginFailedException();
+//            }
+//
 
-            if (!credentials.getPassword().equals(password)) {
-                throw new LoginFailedException();
-            }
+//            int id = (int) (Math.random() * (50000));
+//            sessionRepository.save(new SessionId(id));
+//            return new UserToken(id);
 
-            int id = (int) (Math.random() * (50000));
-            sessionRepository.save(new SessionId(id));
-            return new UserToken(id);
-        }
+        return request.getParameter("json");
+//        }
     }
 }
